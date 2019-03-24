505ac34ae993b44ea372482e3f501743311cf723
James Mwangi <james_mwangi@aol.com>
2019-03-24 16:58:17 +0300

    add file to define app url paths

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url,include
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^signup/$', views.signup, name='signup'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
