1a1b32a88804dbfed8556ef65f1a850c53ae8555
James Mwangi <james_mwangi@aol.com>
2019-03-24 19:00:54 +0300

    update files according to migration

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^signup/$', views.home, name='signup'),
 ]
 
 if settings.DEBUG:
